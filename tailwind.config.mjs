3a581fd50f5de40c5a41353af0174176b84f4ab9
Paulanerus <41129807+Paulanerus@users.noreply.github.com>
2024-09-12 09:41:23 +0200

    refactor: Update theme colors in tailwind.config.mjs

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -3,7 +3,16 @@ export default {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   darkMode: "class",
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "primary-light": "#f9f1f0",
+        "primary-dark": "#161111",
+        "secondary": "#D7C7B4",
+        "text": "#1F1203",
+        "text-test": "#443826",
+        "accent": "#4C4B43",
+      },
+    },
   },
   plugins: [],
 };
